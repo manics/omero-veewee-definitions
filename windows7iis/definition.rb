a4bc1b5e83dc9e9cfea019bb1172da3a35a3839f
Simon Li <spli@dundee.ac.uk>
2014-05-21 23:47:44 +0100

    Replace trial iso with windows7enterprise64bitdvd.iso

--- windows7iis/definition.rb
+++ windows7iis/definition.rb
@@ -3,11 +3,10 @@
 # I'm not sure how to set that with veewee::session yet
 Veewee::Session.declare({
     :os_type_id => 'Windows7_64',
-    # Windows 7 Enterprise 90-day Trial
-    # http://technet.microsoft.com/en-us/evalcenter/cc442495.aspx 
-    :iso_file => "7600.16385.090713-1255_x64fre_enterprise_en-us_EVAL_Eval_Enterprise-GRMCENXEVAL_EN_DVD.iso",
-    :iso_src => "http://wb.dlservice.microsoft.com/dl/download/release/Win7/3/b/a/3bac7d87-8ad2-4b7a-87b3-def36aee35fa/7600.16385.090713-1255_x64fre_enterprise_en-us_EVAL_Eval_Enterprise-GRMCENXEVAL_EN_DVD.iso",
-    :iso_md5 => "1d0d239a252cb53e466d39e752b17c28",
+    # Windows 7 Enterprise
+    :iso_file => "windows7enterprise64bitdvd.iso",
+    :iso_src => "windows7enterprise64bitdvd.iso",
+    :iso_md5 => "8db2fa2c64160f582bd099e86dbe219f",
     :iso_download_timeout => "100000",
     :cpu_count => '1',
     :memory_size=> '512', 
